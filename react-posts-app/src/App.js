b2d06aff42cb46aaf9a17cae7b64391d7a7ab5a6
dibran_ho <dibran_ho@outlook.com>
2020-08-18 15:52:06 +0200

    Modified App.js and created PostsList component

--- react-posts-app/src/App.js
+++ react-posts-app/src/App.js
@@ -1,13 +1,23 @@
-import React from 'react';
+import React from "react";
+import PostsList from './components/PostsList'
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className="posts-app">
-                <h1>Test</h1>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="posts-app">
+        <form>
+          <label>title</label>
+          <input type="text"></input>
+          <label>content</label>
+          <textarea></textarea>
+          <button>Submit</button>
+        </form>
+        <div className="posts-list">
+            <PostsList />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ react-posts-app/src/components/PostsList.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class PostsList extends React.Component {
+  render() {
+    return (
+      <ul>
+        <li>test 1</li>
+        <li>test 2</li>
+      </ul>
+    );
+  }
+}
+
+export default PostsList;
