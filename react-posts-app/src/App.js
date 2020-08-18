22bff19e576262a70fe254c5967c9d313f947658
dibran_ho <dibran_ho@outlook.com>
2020-08-18 15:27:18 +0200

    created index.js and App.js file and  test the app if it works or not! .

---
+++ react-posts-app/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div className="posts-app">
+                <h1>Test</h1>
+            </div>
+        )
+    }
+}
+
+export default App;
